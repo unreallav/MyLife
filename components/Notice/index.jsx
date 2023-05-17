6332b885d1c7bc81ee0a8396a4a94f0afefa1a3e
unreallav <83292446+unreallav@users.noreply.github.com>
2023-05-17 14:05:13 +0800

    index.jsx

--- components/Notice/index.jsx
+++ components/Notice/index.jsx
@@ -24,20 +24,8 @@ const Notice = () => {
                 <p style={{
                     color: '#666',
                     marginBottom: '10px'
-                }}>GAS越大 鱼越贵!</p>
-                <p style={{color: '#666', marginBottom: '10px'}}>为什么要这样做？</p>
-//                 <ul>
-//                     <li style={{color: '#666', marginBottom: '10px'}}>1.
-//                         由于服务器和域名原因，每次大家都得输入地址才能访问，不方便。
-//                     </li>
-//                     <li style={{color: '#666', marginBottom: '10px'}}>2. 迁移到Github以后，大家可以直接通过域名访问。</li>
-//                     <li style={{color: '#666', marginBottom: '10px'}}>3.
-//                         迁移以后，这款工具依旧是永久免费和永久开源的。永远都不会收费。
-//                     </li>
-//                     <li style={{color: '#666', marginBottom: '10px'}}>4.
-//                         迁移以后，能够更快的访问网页，不会出现卡顿或者打不开网页的情况。
-//                     </li>
-//                 </ul>
+                }}>         GAS越大 鱼越贵!          </p>
+               
                
                 <p>本程序源自北北 开源地址:<a
                     href={"https://github.com/wxtsky/MyWalletScan"}>https://github.com/wxtsky/MyWalletScan</a>
